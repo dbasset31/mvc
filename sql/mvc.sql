fc8119de0abde389f0bbc903b0ede9216b2e2e6b
darkmoi3108 <77158918+darkmoi3108@users.noreply.github.com>
2021-01-20 16:49:56 +0100

    Update mvc.sql

--- sql/mvc.sql
+++ sql/mvc.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2021-01-15 14:17:16
+Date: 2021-01-20 16:49:30
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -56,18 +56,39 @@ DROP TABLE IF EXISTS `news`;
 CREATE TABLE `news` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `titre` varchar(255) NOT NULL,
-  `contenu` varchar(255) NOT NULL,
+  `contenu` longtext NOT NULL,
   `date` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=89 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=127 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of news
 -- ----------------------------
-INSERT INTO `news` VALUES ('87', 'gdrfg', 'gdrgdrgdrfgdrf', '10/01/2021');
-INSERT INTO `news` VALUES ('84', 'new 1', 'test\r\n', '10/01/2021');
-INSERT INTO `news` VALUES ('81', 'new 1147', '            test147\r\n            ', '10/01/2021');
-INSERT INTO `news` VALUES ('88', 'test 147', 'fdfstsdtg', '14/01/2021');
+INSERT INTO `news` VALUES ('115', 'xcbvdfg', 'dsfgsegfdesgedsg', '18/01/2021');
+INSERT INTO `news` VALUES ('116', 'setgg', '       gdgef                             dsvds                        grdggrd                                                            ', '20/01/2021');
+INSERT INTO `news` VALUES ('118', 'dgerg', 'tge', '20/01/2021');
+INSERT INTO `news` VALUES ('119', 'gergre', '            egfsgsegdsgsdg esfesf esf ef efqz qz fesf fesf esf esse fsefÂ rdgdrgrdg            ', '20/01/2021');
+INSERT INTO `news` VALUES ('121', 'rdhdfdfg', '<p>dgdfg</p>', '20/01/2021');
+INSERT INTO `news` VALUES ('122', 'dgesgsgsgr', '<p>vdss</p><p>dsfdsg</p><p>sgsfs</p><p>svessd</p><h2 style=\"text-align: center; \"><font color=\"#ff0000\" style=\"background-color: rgb(0, 255, 255);\"><u>Coucou</u></font></h2><p style=\"text-align: right;\"></p>', '20/01/2021');
+INSERT INTO `news` VALUES ('123', 'dsggdgdrg', '<h1><font color=\"#ffff00\"><span style=\"font-family: Impact;\">drhdhtjfhft</span></font></h1>', '20/01/2021');
+
+-- ----------------------------
+-- Table structure for `pages`
+-- ----------------------------
+DROP TABLE IF EXISTS `pages`;
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) NOT NULL,
+  `contenu` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `admin` int(11) NOT NULL,
+  `connected` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of pages
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for `users`
@@ -88,12 +109,12 @@ CREATE TABLE `users` (
   `avatar` varchar(255) NOT NULL,
   `connected` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=84 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES ('72', 'admin', '$2y$10$hpqW.kejgehLDFoBZb9hneD5eUrJ.UW9Yq0i/FMvjsPOy8HfM3EaG', 'admin@topheberge.fr', 'Darkmoi31', 'Homme', '1', 'anonyme', 'Anonyme', '1995-08-31', '16 Nov 2020 16:43:34', '/uploads/avatars/Alliance_WoW.png', '1');
+INSERT INTO `users` VALUES ('72', 'admin', '$2y$10$hpqW.kejgehLDFoBZb9hneD5eUrJ.UW9Yq0i/FMvjsPOy8HfM3EaG', 'admin@topheberge.fr', 'Darkmoi147', 'Homme', '1', 'anonyme', 'Anonymesc', '1995-08-31', '16 Nov 2020 16:43:34', '/uploads/avatars/BV2BOK2CS6GLRHM5BUZSD7FF5Q.jpg', '1');
 
 -- ----------------------------
 -- Table structure for `web_config`
