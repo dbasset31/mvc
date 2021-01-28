d8e31f80a2d47b1d0ac63409e47f278a94edec05
Donovan <admin@donovan-basset.fr>
2021-01-28 19:02:36 +0100

    Update compte.php

--- vue/utilisateur/compte.php
+++ vue/utilisateur/compte.php
@@ -10,35 +10,36 @@
         </header>
         <main>
             <div class="row justify-content-around">
-                <div class = "pr-0 pl-0 mt-5 col-lg-7 col-md-12 mb-5 ombre">
-                    <div class="carte-head w-100">
-                        <h2>Mon espace membre</h2>
-                    </div>
-                    <?php $date = date_create($data->naissance); ?>
-                    <div class="row d-flex  mr-0 ml-0 pl-0 pr-0 align-items-center carte">
-                        <div class="col-md-8  mt-3 align-items-center justify-content-center ml-3">
-                            <p>Bienvenue dans votre compte <b><?php echo strtoupper($data->nom); echo " ".$data->prenom." !";?></b> Voici vos informations</p>
-                            <p>Nom : <b><?php echo $data->nom;?></b></p>
-                            <p>Prénom : <b><?php echo $data->prenom;?></b></p>
-                            <p>Pseudo : <b><?php echo $data->pseudo;?></b></p>
-                            <p>Sexe : <b><?php echo $data->sexe;?></b></p>
-                            <p>Date de naissance : <b><?php echo date_format($date, "d M Y");?></b></p>
-                            <p>Email : <b><?php echo $data->email;?></b></p>
-                            <p>Vous êtes inscrit depuis le : <b><?php echo $data->inscription;?></b></p>
-                            <p>Le solde de votre compte est de : <b><?php echo $data->solde;?> €.</b></p>
-                            <!-- <p>info : <?php //echo $data->info(); ?> -->
-                            <p class="pt-3"><a class = "btn btn-sm btn-success" href = "modifier"><i class = "glyphicon glyphicon-edit"></i> Modifier votre profil</a>
-                                <a class = "btn btn-sm btn-info" href = "logout"><i class = "glyphicon glyphicon-off"></i> Déconnexion</a></p>
+                <div class = "pr-0 pl-0 mt-5 col-lg-7 col-md-12 mb-5">
+                    <div class="ombre">
+                        <div class="carte-head w-100">
+                            <h2>Mon espace membre</h2>
                         </div>
-                        <div class="col-md-3 avatar-compte d-flex justify-content-end">
-                            <div class="column d-flex">
-                            <img class="avatar-img" src="<?php echo $data->avatar ?>">
-                            <a class = "btn btn-sm btn-success mt-2" href = "avatar"><i class = "glyphicon glyphicon-edit"></i> Modifier votre avatar</a>
+                        <?php $date = date_create($data->naissance); ?>
+                        <div class="row d-flex  mr-0 ml-0 pl-0 pr-0 align-items-center carte">
+                            <div class="col-md-8  mt-3 align-items-center justify-content-center ml-3">
+                                <p>Bienvenue dans votre compte <b><?php echo strtoupper($data->nom); echo " ".$data->prenom." !";?></b> Voici vos informations</p>
+                                <p>Nom : <b><?php echo $data->nom;?></b></p>
+                                <p>Prénom : <b><?php echo $data->prenom;?></b></p>
+                                <p>Pseudo : <b><?php echo $data->pseudo;?></b></p>
+                                <p>Sexe : <b><?php echo $data->sexe;?></b></p>
+                                <p>Date de naissance : <b><?php echo date_format($date, "d M Y");?></b></p>
+                                <p>Email : <b><?php echo $data->email;?></b></p>
+                                <p>Vous êtes inscrit depuis le : <b><?php echo $data->inscription;?></b></p>
+                                <p>Le solde de votre compte est de : <b><?php echo $data->solde;?> €.</b></p>
+                                <!-- <p>info : <?php //echo $data->info(); ?> -->
+                                <p class="pt-3"><a class = "btn btn-sm btn-success" href = "modifier"><i class = "glyphicon glyphicon-edit"></i> Modifier votre profil</a>
+                                    <a class = "btn btn-sm btn-info" href = "logout"><i class = "glyphicon glyphicon-off"></i> Déconnexion</a></p>
+                            </div>
+                            <div class="col-md-3 avatar-compte d-flex justify-content-end">
+                                <div class="column d-flex">
+                                    <img class="avatar-img" src="<?php echo $data->avatar ?>">
+                                    <a class = "btn btn-sm btn-success mt-2" href = "avatar"><i class = "glyphicon glyphicon-edit"></i> Modifier votre avatar</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
                 <div class="col-lg-3 col-md-12 mb-0">
                     <?php include "tchat.php"; ?>
                 </div>
